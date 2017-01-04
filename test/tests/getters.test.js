e496d49a24f591ef2a6bebf8f9d623ef8abd595e
Francesco Cioria <firefelix@gmail.com>
2017-01-04 20:49:17 +0100

    add test to verify that "warn" does not log anything in production

--- test/tests/getters.test.js
+++ test/tests/getters.test.js
@@ -34,4 +34,25 @@ describe('Getters', () => {
     expect(lastLoggedWarning).toBe('You shouldn\'t use LocalDate.getMilliseconds as LocalDate is time agnostic.');
   });
 
+  it('"warn" should not log a warning if process.node.env is "production"', () => {
+    const _NODE_ENV = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+    lastLoggedWarning = null;
+
+    expect(localDate.getHours()).toBe(0);
+    expect(lastLoggedWarning).toBe(null);
+
+    expect(localDate.getMinutes()).toBe(0);
+    expect(lastLoggedWarning).toBe(null);
+
+    expect(localDate.getSeconds()).toBe(0);
+    expect(lastLoggedWarning).toBe(null);
+
+    expect(localDate.getMilliseconds()).toBe(0);
+    expect(lastLoggedWarning).toBe(null);
+
+    // restore NODE_ENV
+    process.env.NODE_ENV = _NODE_ENV;
+  });
+
 });
