bbecba77cf74120fd5c82cace5002aeea19a6fad
Francesco Cioria <firefelix@gmail.com>
2017-01-04 20:45:32 +0100

    log warning when user calls getHours/Minutes/Seconds/Milliseconds

--- src/LocalDate.js
+++ src/LocalDate.js
@@ -4,6 +4,12 @@ function pad2(number) {
   return number < 10  ? `0${number}` : number;
 }
 
+function warn(message) {
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn(message); // eslint-disable-line no-console
+  }
+}
+
 export default class LocalDate extends Date {
 
   constructor(value = new Date()) {
@@ -21,6 +27,26 @@ export default class LocalDate extends Date {
 
   toISOString = () => {
     return [this.getFullYear(), pad2(this.getMonth() + 1), pad2(this.getDate())].join('-');
+  }
+
+  getHours = () => {
+    warn('You shouldn\'t use LocalDate.getHours as LocalDate is time agnostic.');
+    return Date.prototype.getHours.call(this);
+  }
+
+  getMinutes = () => {
+    warn('You shouldn\'t use LocalDate.getMinutes as LocalDate is time agnostic.');
+    return Date.prototype.getMinutes.call(this);
+  }
+
+  getSeconds = () => {
+    warn('You shouldn\'t use LocalDate.getSeconds as LocalDate is time agnostic.');
+    return Date.prototype.getSeconds.call(this);
+  }
+
+  getMilliseconds = () => {
+    warn('You shouldn\'t use LocalDate.getMilliseconds as LocalDate is time agnostic.');
+    return Date.prototype.getMilliseconds.call(this);
   }
 
 }
